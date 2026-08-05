c596d88e65bb702eecfbd1a1b4cf8fc3f513df35
Vitalii Hoshuliak <goshulyakvitalik@gmail.com>
2026-08-05 20:43:42 +0100

    in progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable jsx-a11y/control-has-associated-label */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { UserWarning } from './UserWarning';
-import { USER_ID } from './api/todos';
+import { getTodos, USER_ID } from './api/todos';
 
 export const App: React.FC = () => {
   if (!USER_ID) {
     return <UserWarning />;
   }
+  const [todos, setTodos] = useState<Todo[]>([]);
+ useEffect(
+  () => {
+    getTodos().then(loadedTodos => setTodos(loadedTodos));
+  },
+  [],
+);
 
   return (
     <div className="todoapp">
